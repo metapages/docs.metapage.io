c5b689eeb397f22837667b2a3aae4ac7af6ecae7
Dion Amago <dion@transition9.com>
2023-09-13 21:47:46 -0700

    Custom plugins are now in a published module

--- docu-notion.config.ts
+++ docu-notion.config.ts
@@ -1,20 +1,14 @@
 import {
+  correctNotionUrlsInMermaid,
+  modifiedStandardInternalLinkConversion,
+  notionColumnsUpgraded,
+} from '@metapages/docu-notion-plugins';
+import {
   IDocuNotionConfig,
   Log,
 } from '@sillsdev/docu-notion';
 
-import {
-  correctNotionUrlsInMermaid,
-} from './src/custom-docu-notion-plugins/correctNotionUrlsInMermaid';
-import {
-  modifiedStandardInternalLinkConversion,
-} from './src/custom-docu-notion-plugins/modifiedInternalLinks';
-import {
-  notionColumnsUpgraded,
-} from './src/custom-docu-notion-plugins/notionColumnsUpgraded';
-
 Log.setLogLevel("verbose");
-
 
 const config: IDocuNotionConfig = {
   plugins: [
