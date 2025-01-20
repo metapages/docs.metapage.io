eb5e1d73ae9ed50e2bf0d74c4c4032d9400cd150
metapage CI Robot <robot@metapage.io>
2025-01-19 21:57:55 -0800

    checkpoint

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -38,7 +38,11 @@ const config :Config = {
     mermaid: true,
   },
 
-  themes: ["@docusaurus/theme-mermaid", "@docusaurus/theme-live-codeblock"],
+  themes: [
+    "@docusaurus/theme-mermaid",
+    "@docusaurus/theme-live-codeblock",
+    "docusaurus-theme-search-typesense"
+  ],
 
   presets: [
     [
@@ -73,6 +77,38 @@ const config :Config = {
 
   themeConfig:
     ({
+      typesense: {
+        // Replace this with the name of your index/collection.
+        // It should match the "index_name" entry in the scraper's "config.json" file.
+        typesenseCollectionName: 'docs-metapage-io',
+  
+        typesenseServerConfig: {
+          nodes: [
+            {
+              host: 'xxx-1.a1.typesense.net',
+              port: 443,
+              protocol: 'https',
+            },
+            {
+              host: 'xxx-2.a1.typesense.net',
+              port: 443,
+              protocol: 'https',
+            },
+            {
+              host: 'xxx-3.a1.typesense.net',
+              port: 443,
+              protocol: 'https',
+            },
+          ],
+          apiKey: 'xyz',
+        },
+  
+        // Optional: Typesense search parameters: https://typesense.org/docs/0.24.0/api/search.html#search-parameters
+        typesenseSearchParameters: {},
+  
+        // Optional
+        contextualSearch: true,
+      },
       mermaid: {
         theme: {
           light: "neutral",
